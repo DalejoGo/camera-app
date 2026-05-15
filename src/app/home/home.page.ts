04c1d8c5e691c4d11eee5462ef0ab7dd0de30e58
DalejoGo <dacty2003@gmail.com>
2026-05-15 07:52:34 -0500

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  foto: string | undefined;
+
+  constructor() {}
+
+  async tomarFoto() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Camera
+      });
+
+      this.foto = image.dataUrl;
+      
+    } catch (error) {
+      console.error('Error al tomar la foto', error);
+    }
+  }
+}
